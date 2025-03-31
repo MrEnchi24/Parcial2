b5157ea7c2bb3fdd6ee08018742febd80d86ca69
alex0626 <alexperezmillan26@gmail.com>
2025-03-31 11:25:04 -0600

    act al programa

---
+++ Programa_Principal/Programa_Principal.py
@@ -0,0 +1,21 @@
+def menu():
+    while True:
+        print("Bienvenido al programa donde vas a poder consultar distintas cosas acerca de tu semestre de la universidad en la UACH")
+        print("A continuación se muestran las opciones a las que puede acceder: ")
+        print("Opcion 1: Promedio de tus calificaciones:")
+        print("Opicón 2: Tus asistencias a clases durante el semestre")
+        print("Opción 3: Riesgo que tienes de ser dado de bajo por tus No Acreditadas")
+        print("Opción 4: Cuanto te falta para aprobar el semestre, considerando la calificación mínima como 7")
+        opcion= int("Selecciona la opción que deseas seleccionar hoy: ")
+        match(opcion):
+          
+
+
+
+        break
+menu()
+
+
+
+if __name__=="__menu__":
+  menu()
