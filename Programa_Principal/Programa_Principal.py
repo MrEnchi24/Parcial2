cc320a2bf69ffa886d7958f67e65b056643e8e4b
alex0626 <alexperezmillan26@gmail.com>
2025-03-31 23:32:48 -0600

    cambios en todas las funciones y menus

--- Programa_Principal/Programa_Principal.py
+++ Programa_Principal/Programa_Principal.py
@@ -1,26 +1,41 @@
 from menus import media
+def asistencias():
+   semestre= int(input("En que semestre estas actualmente:"))
+   na= int(input("Cuantas materias no acreditadas tienes?: "))
+  
+  
+  
 
 
 def menu():
-    while True:
-        print("Bienvenido al programa donde vas a poder consultar distintas cosas acerca de tu semestre de la universidad en la UACH")
-        print("A continuación se muestran las opciones a las que puede acceder: ")
-        print("Opcion 1: Promedio de tus calificaciones:")
-        print("Opicón 2: Tus asistencias a clases durante el semestre")
-        print("Opción 3: Riesgo que tienes de ser dado de bajo por tus No Acreditadas")
-        print("Opción 4: Cuanto te falta para aprobar el semestre, considerando la calificación mínima como 7")
-        opcion= int("Selecciona la opción que deseas seleccionar hoy: ")
-        match(opcion):
-           case(1):
-              media()
-          
+  while True:
+    print("\nBienvenido al programa donde vas a poder consultar distintas cosas acerca de tu semestre de la universidad en la UACH")
+    print("A continuación se muestran las opciones a las que puede acceder: ")
+    print("Opcion 1: Promedio de tus calificaciones de todo el semestre:")
+    print("Opicón 2: Tus asistencias a clases durante el semestre")
+    print("Opción 3: Riesgo que tienes de ser dado de bajo por tus No Acreditadas")
+    print("Opción 4: Cuanto te falta para aprobar el semestre, considerando la calificación mínima como 7")
+    opcion= int(input("Selecciona la opción que deseas seleccionar hoy: "))
+    match(opcion):
+      case(1):
+        media()
+        continuar= input("\n¿Deseas hacer algo más? (s/n): ").lower()
+        if continuar != "s":
+          print("Gracias por usar el programa!!")
+          break
+      case(2):
+        asistencias()
+        continuar= input("\n¿Deseas hacer algo más? (s/n): ").lower()
+        if continuar != "s":
+          print("Gracias por usar el programa!!")
+          break
+         
+           
 
 
 
-        break
-menu()
+        
 
 
-
-if __name__=="__menu__":
+if __name__=="__main__":
   menu()
--- Programa_Principal/menus.py
+++ Programa_Principal/menus.py
@@ -1,5 +1,49 @@
-def media(num_1=(int(input("Escribe la primera calificación: "))), 
-          num_2=int(input("Escribe la segunda calificación: ")), num_3=int(input("Escribe la tercera calificación: "))):
-    resultado= (num_1+num_2+num_3)/3
-    return(resultado)
-print("El resultado de tus calificaiones es: ", media())
+def media(): 
+    while True:
+        try:
+            calificacion_1 = float(input("Ingresa tu calificación del primer parcial: "))
+            if 0 <= calificacion_1 <= 10:
+                break  
+            else:
+                print("La calificación debe estar entre 0 y 10")
+        except ValueError:
+            print("Ingresa un valor válido")
+    
+    while True:
+        try:
+            calificacion_2 = float(input("Ingresa tu calificación del segundo parcial: "))
+            if 0 <= calificacion_2 <= 10:
+                break  
+            else:
+                print("La calificación debe estar entre 0 y 10")
+        except ValueError:
+            print("Ingresa un valor válido")
+    
+    while True:
+        try:
+            calificacion_3 = float(input("Ingresa tu calificación del tercer parcial: "))
+            if 0 <= calificacion_3 <= 10:
+                break  
+            else:
+                print("La calificación debe estar entre 0 y 10")
+        except ValueError:
+            print("Ingresa un valor válido")
+    
+    promedio = (calificacion_1 * .30) + (calificacion_2 * .30) + (calificacion_3 * .40)
+    print("Tu promedio es de: ", promedio)
+    if promedio==10:
+        print("WOOOOOOOOW sacaste calificación perfecta \U0001F44C \U0001F973")
+    elif 9 <= promedio <10:
+        print("Felicidades sacaste muy buena calificación \U0001F606")
+    elif 8 <= promedio <8.999:
+        print("Muy bien sigue siendo una buena calificación \U0001F60E ")
+    elif 7.2 <= promedio <7.999:
+        print("Uffff se pasó con eso nos vamos felices \U0001F979 ")
+    elif 7<= promedio<=7.1:
+        print("AYYYYY apenitas pero se pudooooooooo!! \U0001F47B ")
+    elif 6.7<= promedio<=6.9:
+        print("Digale a su profe que no sea gacho y lo paseeee \U0001F62D")
+    else:
+        print("Ni modo no pasa nada, a echarle ganas al extra o a recursar la materia \U0001F642 ")
+    
+    return promedio
